1fe4c9a506e2da4bec141735173b8b38a52e0c78
Daria Zhigalko <daria.zhigalko@yandex.ru>
2023-08-20 18:39:56 +0300

    fix tests

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const { NotFoundError } = require('./utils/errors');
 const {
   PORT = 3000,
   DB_URL = 'mongodb://localhost:27017/mestodb',
-  DB_USER = 'admin',
-  DB_PASSWORD = 'admin',
   CURRENT_USER_ID = '64e21cd2e71c44d866f1fa26',
 } = process.env;
 
@@ -28,7 +26,7 @@ app.use('/cards', cardsRouter);
 
 app.use((err, req, res, _) => {
   if (err instanceof mongoose.Error.ValidationError) {
-    res.status(400).send(err.errors);
+    res.status(400).send({ message: 'Validation error', fields: err.errors });
   } else if (err instanceof NotFoundError) {
     res.status(404).send({ message: err.message });
   } else {
@@ -38,9 +36,6 @@ app.use((err, req, res, _) => {
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
-  authSource: 'admin',
-  user: DB_USER,
-  pass: DB_PASSWORD,
 });
 
 app.listen(PORT, () => {
